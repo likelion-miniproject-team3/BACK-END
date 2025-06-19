65c632125c1982656999a304a4bdd5f4d60c2f81
na-gyeongwon <heyzel77@g.eulji.ac.kr>
2025-06-19 20:14:02 +0900

    기본

---
+++ src/main/java/com/example/majorapp/MajorappApplication.java
@@ -0,0 +1,13 @@
+package com.example.majorapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MajorappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MajorappApplication.class, args);
+	}
+
+}
